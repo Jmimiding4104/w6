3e57086f2cef61104bad63d0e7514f078220815b
Jmimiding4104 <Jmimiding4104@gmail.com>
2022-09-04 02:50:40 +0800

    Update 2022-09-03T18:50:29.398Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,22 +1,23 @@
+"use strict";
+
 $(function () {
   console.log('Hello Bootstrap5');
 });
-
 var swiper = new Swiper(".mySwiper", {
   cssMode: true,
   navigation: {
     nextEl: ".swiper-button-next-self",
-    prevEl: ".swiper-button-prev-self",
+    prevEl: ".swiper-button-prev-self"
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination"
   },
   mousewheel: true,
   keyboard: true,
-  spaceBetween: 100,
+  spaceBetween: 100
 });
-
 $('.nav-list').click(function (e) {
   e.preventDefault();
-  $('.big').toggleClass('active')
-})
+  $('.big').toggleClass('active');
+});
+//# sourceMappingURL=all.js.map
