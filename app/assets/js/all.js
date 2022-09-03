1916a95cb84ce389dab609025cae73b3d00be626
Jmimiding4104 <Jmimiding4104@gmail.com>
2022-09-04 02:41:48 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,22 @@
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next-self",
+    prevEl: ".swiper-button-prev-self",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+  spaceBetween: 100,
+});
+
+$('.nav-list').click(function (e) {
+  e.preventDefault();
+  $('.big').toggleClass('active')
+})
